17021d2ebbd43d726406b27984894646a567e34f
Abhinav <abhinavsrivastav699@gmail.com>
2023-04-21 00:02:39 +0530

    Single page frontend

--- client/src/components/singlePageAccomodation/SingleAccomodation.jsx
+++ client/src/components/singlePageAccomodation/SingleAccomodation.jsx
@@ -1,17 +1,79 @@
-import React, { useEffect } from 'react'
-import {useParams} from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import {Link, useParams} from 'react-router-dom';
 import axios from 'axios'
+import { upperCase, capitalize } from 'lodash';
+import { ShareIcon, WishlistIcon } from '../../images/SVG';
 
 const SingleAccomodation = () => {
     const {id} = useParams();
+    // const [accomodation, setAccomodation] = useState(null);
+    const [accomodation, setAccomodation] = useState(null);
     useEffect(()=>{
-      axios.get(`/accomodation/${id}`).then(data=>{
-        console.log(data)
-      })
+      const getData = async()=>{
+        if(!id) return;
+        try{
+          const {data} = await (axios.get(`/accomodation/${id}`));
+          const {title, description, perks, pricePerNight, extraInfo, address, checkIn, checkOut, maxGuests, photos} = data;
+          setAccomodation({title, description, perks, pricePerNight, extraInfo, address, checkIn, checkOut, maxGuests, photos});
+        }catch(error){
+          console.log(error);
+          alert("Some error occured while loading the accomodation, please refresh the page")
+        }
+      }
+      getData();
     }, [id])
+
+    if(!accomodation) return <div>LOL</div>
+
+    // let title, address, perks, photos, description, checkIn, checkOut, maxGuests, pricePerNight;
+
+    // if(accomodation){
+    //   title = upperCase(accomodation.title);
+    //   description = capitalize(accomodation.description);
+    //   perks = accomodation.perks;
+    //   photos = accomodation.photos;
+    //   checkIn = accomodation.checkIn;
+    //   checkOut = accomodation.checkOut;
+    //   maxGuests = accomodation.maxGuests;
+    //   address = accomodation.address;
+    //   pricePEr
+    // }
+
+
   return (
-    <div>
-      single accomodation {id}
+    <div className='mt-8'>
+      <h1 className='text-2xl text-justify [word-spacing:.4rem] tracking-wide'>{upperCase(accomodation.title)}</h1>
+      <div className='flex justify-between py-1'>
+        <a className='font-medium underline cursor-pointer' target='_blank' rel='noreferrer'  href={`https://maps.google.com/?q=${accomodation.address.city}`}> {capitalize(accomodation.address.city)}, {capitalize(accomodation.address.country)}  </a>
+        <div className='flex gap-8'>
+          <span className='flex gap-1 items-center px-6 py-2 rounded-lg transition-all hover:bg-gray-100'>
+            {ShareIcon}
+            <p className='underline'>Share</p>
+          </span>
+          <span className='flex gap-1 items-center px-6 py-2 rounded-lg transition-all hover:bg-gray-100'>  
+            {WishlistIcon}
+            <p className='underline'>Wishlist</p>
+          </span>  
+        </div>
+      </div>
+
+      <Link to={'/:id/photos'} className="grid grid-cols-[2fr_1fr_1fr] grid-rows-2">
+        <div className='row-span-2 mr-2 my-0'>
+          <img className='h-full w-full object-cover' src={`http://localhost:4000/uploads/${accomodation.photos[0]}`} alt="" />
+        </div>
+        <div className='mb-1 mr-1'>
+          <img className='aspect-square object-cover' src={`http://localhost:4000/uploads/${accomodation.photos[0]}`} alt="" />
+        </div>
+        <div className='ml-1 mb-1'>
+          <img className='aspect-square object-cover' src={`http://localhost:4000/uploads/${accomodation.photos[0]}`} alt="" />
+        </div>
+        <div className='col-start-2 mt-1 mr-1'>
+          <img className='aspect-square object-cover' src={`http://localhost:4000/uploads/${accomodation.photos[0]}`} alt="" />
+        </div>
+        <div className='ml-1 mt-1'>
+          <img className='aspect-square object-cover' src={`http://localhost:4000/uploads/${accomodation.photos[0]}`} alt="" />
+        </div>
+      </Link>
     </div>
   )
 }
--- client/src/images/SVG.jsx
+++ client/src/images/SVG.jsx
@@ -168,5 +168,18 @@ export const EntryIcon =
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
 </svg>
 
+export const ShareIcon = 
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
+</svg>
+
+export const WishlistIcon = 
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+</svg>
+
+
+
+
 
 

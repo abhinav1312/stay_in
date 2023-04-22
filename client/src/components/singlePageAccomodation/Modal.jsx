b29adebe4adaeafa364baf295bc2073140bb0dc8
Abhinav <abhinavsrivastav699@gmail.com>
2023-04-22 23:39:58 +0530

    continuting single page frontend

--- client/src/components/singlePageAccomodation/Modal.jsx
+++ client/src/components/singlePageAccomodation/Modal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Modal = ({setShowModal}) => {
   return (
-    <div className='absolute inset-0  bg-black opacity-60'>
+    <div className='absolute inset-0 top-[-215%]  bg-black opacity-60'>
         <div className='w-2xl h-2xl bg-white p-4 opacity-100 text-black'>
             <h1>Hello</h1>
         </div>

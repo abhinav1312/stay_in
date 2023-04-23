9a5ad810e37a6f5a0216c15fa60045f5c7e033c6
Abhinav <abhinavsrivastav699@gmail.com>
2023-04-24 00:35:53 +0530

    backend integration and code refactoring

--- client/src/components/singlePageAccomodation/Modal.jsx
+++ client/src/components/singlePageAccomodation/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({setShowModal}) => {
   return (
     <div className='absolute inset-0 top-[-215%]  bg-black opacity-60'>
         <div className='w-2xl h-2xl bg-white p-4 opacity-100 text-black'>
-            <h1>Hello</h1>
+            <h1>Hello world</h1>
         </div>
       <button className='text-white' onClick={()=>setShowModal(false)}>Click me</button>
     </div>
